2945941ff1ea1636a63cfb89937156091e600dad
Thiago Tranquilim <trtranquilim@gmail.com>
2023-06-19 15:49:05 -0300

    Delete APPC directory

--- APPC/ExercíciosIf/Exercicio01.py
+++
@@ -1,5 +0,0 @@
-n1 = int(input())
-if(n1 < 0):
-    print('n1 é negativo')
-elif(n1 % 2 == 0):
-    print('n1 é par')
--- APPC/ExercíciosIf/Exercicio02.py
+++
@@ -1,8 +0,0 @@
-A = int(input())
-B = int(input())
-if(A == B):
-    C = A + B
-    print(C)
-else:
-    C = A * B
-    print(C)
